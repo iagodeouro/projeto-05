7b6724ab2798353e04d33812ababdf0b2b230af4
iagodeouro <iagodeouro924@gmail.com>
2023-11-12 17:07:14 -0300

    criação da página de vendas

---
+++ js/menuScroll.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    /*alert('funcionando');*/
+
+    $('nav a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop':offSetTop});
+
+        return false;
+    })
+
+   
+
+    
+
+
+    
+
+    
+   
+
+
+
+
+
+
+});
