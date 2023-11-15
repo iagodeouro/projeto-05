e471dfbbcb1010d748246839bdb88ee52366886b
iagodeouro <iagodeouro924@gmail.com>
2023-11-15 16:37:45 -0300

    veiculos especiais finalizada com html e css - a espera do javascript

--- js/menuScroll.js
+++ js/menuScroll.js
@@ -11,6 +11,14 @@ $(function(){
         return false;
     })
 
+    $('.descricao-veiculo div a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({'scrollTop':offSetTop});
+
+        return false;
+    })
    
 
     
